55c705f427d2eb2033e97b011a1ceec785f21e9a
Lucas Taliberti de Sousa <lucastaliberti@syllus.com.br>
2017-08-05 13:11:06 -0300

    removing bootstrap template and unused import

--- src/index.js
+++ src/index.js
@@ -2,11 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { HashRouter } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap/dist/css/bootstrap-theme.css'
 
 import 'font-awesome/css/font-awesome.css'
 
-// import Routes from './app/Routes'
 import App from './app/App'
 import registerServiceWorker from './registerServiceWorker'
 
