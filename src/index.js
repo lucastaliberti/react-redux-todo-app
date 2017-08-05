24812586c62adf513f2e0687e6538138ad61f128
Lucas Taliberti de Sousa <lucastaliberti@syllus.com.br>
2017-08-05 17:40:21 -0300

    Adding redux

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { HashRouter } from 'react-router-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 import 'bootstrap/dist/css/bootstrap.css'
 
 import 'font-awesome/css/font-awesome.css'
 
 import App from './app/App'
+import reducers from './app/reducers'
 import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<HashRouter><App /></HashRouter>, document.getElementById('root'))
+const store = createStore(reducers)
+
+const AppToRender = () => (
+  <Provider store={store}>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </Provider>
+)
+ReactDOM.render(<AppToRender />, document.getElementById('root'))
 registerServiceWorker()
