8d48471e5a9b3801bebfc70d90df93c94ce47edc
Lucas Taliberti de Sousa <lucastaliberti@syllus.com.br>
2017-08-06 12:42:38 -0300

    changing action type to constants and exporting them along with the initial state

--- src/todo/todoReducer.js
+++ src/todo/todoReducer.js
@@ -1,12 +1,15 @@
-const INITIAL_STATE = { description: '', list: [] }
+export const INITIAL_STATE = { description: '', list: [] }
+export const TODO_DESCRIPTION_CHANGED = 'TODO/DESCRIPTION_CHANGED'
+export const TODO_SEARCHED = 'TODO/TODO_SEARCHED'
+export const TODO_CLEAR = 'TODO/TODO_CLEAR'
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
-    case 'TODO/DESCRIPTION_CHANGED':
+    case TODO_DESCRIPTION_CHANGED:
       return {...state, description: action.payload}
-    case 'TODO/TODO_SEARCHED':
+    case TODO_SEARCHED:
       return {...state, list: action.payload}
-    case 'TODO/TODO_CLEAR':
+    case TODO_CLEAR:
       return {...state, description: ''}
     default:
       return state
