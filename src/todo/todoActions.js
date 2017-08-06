d6e21f3f8cc546017fb9810a30dd704aa754ac62
Lucas Taliberti de Sousa <lucastaliberti@syllus.com.br>
2017-08-06 20:09:51 -0300

    Returning the promises for testing purpose and removed a duplicated search in add method

--- src/todo/todoActions.js
+++ src/todo/todoActions.js
@@ -17,31 +17,32 @@ export const search = () => (dispatch, getState) => {
   const description = getState().todo.description
   const search = description ? `&description__regex=/${description}/i` : ''
 
-  axios.get(`${URL}?sort=-createdAt${search}`)
-    .then(resp => dispatch({type: TODO_SEARCHED, payload: resp.data}))
+  return axios.get(`${URL}?sort=-createdAt${search}`)
+    .then(resp => dispatch(todoSearched(resp.data)))
 }
 
-export const add = description => dispatch => {
+const todoSearched = (body) => ({
+  type: TODO_SEARCHED,
+  payload: body
+})
+
+export const add = description => dispatch =>
   axios.post(URL, {description})
     .then(resp => dispatch(clear()))
-    .then(resp => dispatch(search()))
-}
 
-export const markAsDone = todo => dispatch => {
+export const markAsDone = todo => dispatch =>
   axios.put(`${URL}/${todo._id}`, {...todo, done: true})
     .then(resp => dispatch(search()))
-}
 
-export const markAsPending = todo => dispatch => {
+export const markAsPending = todo => dispatch =>
   axios.put(`${URL}/${todo._id}`, {...todo, done: false})
     .then(resp => dispatch(search()))
-}
 
-export const remove = todo => dispatch => {
+export const remove = todo => dispatch =>
   axios.delete(`${URL}/${todo._id}`)
     .then(resp => dispatch(search()))
-}
 
-export const clear = () => (
-  [{type: TODO_CLEAR}, search()]
-)
+export const clear = () => dispatch => {
+  dispatch({type: TODO_CLEAR})
+  return dispatch(search())
+}
