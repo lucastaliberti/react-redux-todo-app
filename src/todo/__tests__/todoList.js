6cbef6977d97f8cc2ebe1328d6968abfd1ca38ed
Lucas Taliberti de Sousa <lucastaliberti@syllus.com.br>
2017-08-07 09:46:42 -0300

    Fixed TodoList testing

--- src/todo/__tests__/todoList.js
+++ src/todo/__tests__/todoList.js
@@ -1,30 +1,88 @@
 
 import React from 'react'
 import {shallow} from 'enzyme'
-import Todo from '../Todo'
+import {StyleSheetTestUtils} from 'aphrodite'
+import {TodoList, mapStateToProps} from '../TodoList'
 
-import PageHeader from '../../template/PageHeader'
-import TodoForm from '../TodoForm'
-import TodoList from '../TodoList'
+import IconButton from '../../template/IconButton'
+
+const storeStateMock = {
+  todo: {
+    description: 'estudar testes',
+    list: [
+      {
+        '_id': '1',
+        'description': 'teste para estudar',
+        '__v': 0,
+        'createdAt': '2017-08-05T23:48:19.550Z',
+        'done': false
+      }
+    ]
+  }
+}
+
+const setup = (storeStateMock) => {
+  const props = {
+    list: storeStateMock.todo.list,
+    markAsDone: jest.fn(),
+    markAsPending: jest.fn(),
+    remove: jest.fn()
+  }
+
+  const wrapper = shallow(<TodoList {...props} />)
+
+  return {
+    props,
+    wrapper
+  }
+}
+let wrapper = null
+let props = {}
 
 describe('TODO Component', () => {
-  const wrapper = shallow(
-    <Todo />
-  )
+  beforeEach(() => {
+    ({wrapper, props} = setup(storeStateMock))
+    StyleSheetTestUtils.suppressStyleInjection()
+  })
+
+  afterEach(() => {
+    StyleSheetTestUtils.clearBufferAndResumeStyleInjection()
+  })
+
+  it('Should mapStateToProps correctly', () => {
+    const list = storeStateMock.todo.list
+    expect(mapStateToProps(storeStateMock)).toEqual({list})
+  })
 
   it('Should have a div wrapping all', () => {
-    expect(wrapper.find('div')).toHaveLength(1)
+    expect(wrapper.find('Table')).toHaveLength(1)
   })
 
-  it('Should render a PageHeader component', () => {
-    expect(wrapper.find(PageHeader)).toHaveLength(1)
+  it('Should render 3 IconButton', () => {
+    expect(wrapper.find(IconButton).find({hide: false})).toHaveLength(1)
+    expect(wrapper.find(IconButton).find({hide: true})).toHaveLength(2)
   })
 
-  it('Should render a TodoForm component', () => {
-    expect(wrapper.find(TodoForm)).toHaveLength(1)
+  it('Should dispatch markAsDone on click', () => {
+    const IconButtonProps = wrapper.find(IconButton).find({hide: false}).props()
+
+    IconButtonProps.onClick('')
+    expect(props.markAsDone.mock.calls.length).toBe(1)
   })
 
-  it('Should render a TodoList component', () => {
-    expect(wrapper.find(TodoList)).toHaveLength(1)
+  it('Should dispatch markAsPending on click', () => {
+    storeStateMock.todo.list[0].done = true;
+    ({wrapper, props} = setup(storeStateMock))
+    const IconButtonProps = wrapper.find(IconButton).find({hide: false}).get(0).props
+    IconButtonProps.onClick('')
+    expect(props.markAsPending.mock.calls.length).toBe(1)
+  })
+
+  it('Should dispatch remove on click', () => {
+    storeStateMock.todo.list[0].done = true;
+    ({wrapper, props} = setup(storeStateMock))
+    const IconButtonProps = wrapper.find(IconButton).find({hide: false}).get(1).props
+    IconButtonProps.onClick('')
+    expect(props.remove.mock.calls.length).toBe(1)
   })
 })
