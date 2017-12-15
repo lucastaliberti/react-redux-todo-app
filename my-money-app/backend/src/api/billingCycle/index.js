6dc36f4fa5328df97bec05be645aa44becb41000
Lucas Taliberti de Sousa <lucastaliberti@syllus.com.br>
2017-12-15 18:04:06 -0200

    Renaming the schema file to schema and added the routing service as the index file

--- my-money-app/backend/src/api/billingCycle/index.js
+++ my-money-app/backend/src/api/billingCycle/index.js
@@ -1,25 +1,9 @@
-const restful = require("node-restful");
-const mongoose = restful.mongoose;
+const BillingCycle = require('./schema')
 
-const debitEnum = ["PAGO", "PENDENTE", "AGENDADO"];
+BillingCycle.methods(['get', 'post', 'put', 'delete'])
+BillingCycle.updateOptions({
+  new: true,
+  runValidators: true
+})
 
-const creditSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  value: { type: Number, min: 0, required: true }
-});
-
-const debitSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  value: { type: Number, min: 0, required: true },
-  status: { type: String, required: false, uppercase: true, enum: debitEnum }
-});
-
-const billingCycleSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  month: { type: Number, min: 0, max: 12, required: true },
-  year: { type: Number, min: 1970, max: 2100, required: true },
-  credits: [creditSchema],
-  debts: [debitSchema]
-});
-
-module.exports = restful.model("BillingCycle", billingCycleSchema);
+module.exports = BillingCycle
