57de0821e73260a46762ff4503c54a2b980fb4ee
Lucas Taliberti de Sousa <lucastaliberti@syllus.com.br>
2017-12-16 18:43:43 -0200

    Added cors middleware

--- my-money-app/backend/src/config/server.js
+++ my-money-app/backend/src/config/server.js
@@ -3,10 +3,13 @@ const PORT = 3003
 const bodyParser = require('body-parser')
 const express = require('express')
 
+const allowCors = require('./cors')
+
 const server = express()
 
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
+server.use(allowCors)
 
 server.listen(PORT, function() {
   console.log(`BACKEND is running on port ${PORT}`)
