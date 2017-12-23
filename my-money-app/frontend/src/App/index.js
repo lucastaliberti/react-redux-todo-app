5485cf893f6b22c0f479471922f90361e7967a4e
Lucas Taliberti de Sousa <lucastaliberti@syllus.com.br>
2017-12-23 20:49:26 -0200

    Added footer component and a placeholder content

--- my-money-app/frontend/src/App/index.js
+++ my-money-app/frontend/src/App/index.js
@@ -1,14 +1,21 @@
 import React from 'react'
 //import logo from '../logo.svg'
-import { Layout } from 'reactjs-admin-lte'
+import { Layout, Content } from 'reactjs-admin-lte'
 import './index.css'
 import Header from '../components/Header'
 import Sidebar from '../components/Sidebar'
+import Footer from '../components/Footer'
 
 const App = props => (
   <Layout skin="blue" type="fixed">
     <Header />
+    <Content>
+      <Content.Body>
+        <h1>Conteúdo</h1>
+      </Content.Body>
+    </Content>
     <Sidebar />
+    <Footer />
   </Layout>
 )
 
---
+++ my-money-app/frontend/src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { MainFooter } from 'reactjs-admin-lte'
+
+const Footer = () => (
+  <MainFooter>
+    <strong>
+      Copyright &copy; 2017 <a href="www.syllus.com.br">Syllus</a>.
+    </strong>
+    &nbsp;All rights reserved.
+  </MainFooter>
+)
+
+export default Footer
