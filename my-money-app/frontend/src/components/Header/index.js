c839f294e24b1367e5d11a17ab16933789921a50
Lucas Taliberti de Sousa <lucastaliberti@syllus.com.br>
2017-12-24 16:06:08 -0200

    Using fontAwesome component

--- my-money-app/frontend/src/components/Header/index.js
+++ my-money-app/frontend/src/components/Header/index.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import FontAwesome from 'react-fontawesome'
 import { MainHeader } from 'reactjs-admin-lte'
 
 const Header = props => (
   <MainHeader>
     <MainHeader.Logo href="/#/">
       <MainHeader.Logo.Large>
-        <i className="fa fa-money" />
+        <FontAwesome name="money" tag="i" />
         <b>My</b> Money
       </MainHeader.Logo.Large>
     </MainHeader.Logo>
