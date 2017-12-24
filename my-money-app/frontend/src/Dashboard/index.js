3a18704f7d336cfe54169d66c4ec4f13b8556e8c
Lucas Taliberti de Sousa <lucastaliberti@syllus.com.br>
2017-12-24 17:53:37 -0200

    Removing unused grid

--- my-money-app/frontend/src/Dashboard/index.js
+++ my-money-app/frontend/src/Dashboard/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { Content } from 'reactjs-admin-lte'
-import { Grid, Row, Col } from 'react-bootstrap'
+import { Row, Col } from 'react-bootstrap'
 
 import InfoBox from '../components/InfoBox'
 
