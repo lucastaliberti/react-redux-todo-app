2dd7b9e23d9e29f15df870096ec8b43cad18e64c
Lucas Taliberti de Sousa <lucastaliberti@syllus.com.br>
2017-12-24 17:53:26 -0200

    Basic redux setup

---
+++ my-money-app/frontend/src/App/reducers.js
@@ -0,0 +1,12 @@
+import { combineReducers } from 'redux'
+
+const rootReducer = combineReducers({
+  dashboard: () => ({
+    summary: {
+      credit: 100,
+      debit: 50
+    }
+  })
+})
+
+export default rootReducer
