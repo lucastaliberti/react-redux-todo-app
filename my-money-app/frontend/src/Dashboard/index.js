12244ead3c104eaee56048f0f900520b2cd61fa1
Lucas Taliberti de Sousa <lucastaliberti@syllus.com.br>
2017-12-25 14:03:54 -0200

    Added redux to the dashboard component and wired redux and sagas to the app

--- my-money-app/frontend/src/Dashboard/index.js
+++ my-money-app/frontend/src/Dashboard/index.js
@@ -1,11 +1,48 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
 import { Content } from 'reactjs-admin-lte'
 import { Row, Col } from 'react-bootstrap'
 
+import { dataFetch } from './actions'
 import InfoBox from '../components/InfoBox'
 
+const defaultRowSize = { xs: 12, md: 4 }
+const infoBoxData = (color, icon, text, number) => ({
+  row: defaultRowSize,
+  info: {
+    color: color,
+    icon: icon,
+    text: text,
+    number: `R$ ${number}`
+  }
+})
+
+const infoBoxCredit = credit =>
+  infoBoxData('green', 'bank', 'Total de créditos', credit)
+
+const infoBoxDebt = debt =>
+  infoBoxData('red', 'credit-card', 'Total de débitos', debt)
+
+const infoBoxTotal = total =>
+  infoBoxData('blue', 'money', 'Total consolidado', total)
+
 class Dashboard extends Component {
+  componentDidMount() {
+    const { dataFetch } = this.props
+    dataFetch()
+  }
+
   render() {
+    const { summary } = this.props
+    const { credit, debt } = summary
+
+    const infoBoxesToRender = [
+      infoBoxCredit(credit),
+      infoBoxDebt(debt),
+      infoBoxTotal(credit - debt)
+    ]
+
     return (
       <Content>
         <Content.Header>
@@ -15,34 +52,21 @@ class Dashboard extends Component {
         </Content.Header>
         <Content.Body>
           <Row>
-            <Col xs={12} md={4}>
-              <InfoBox
-                color="green"
-                icon="bank"
-                text="Total de créditos"
-                number="R$ 10"
-              />
-            </Col>
-            <Col xs={12} md={4}>
-              <InfoBox
-                color="red"
-                icon="credit-card"
-                text="Total de débitos"
-                number="R$ 10"
-              />
-            </Col>
-            <Col xs={12} md={4}>
-              <InfoBox
-                color="blue"
-                icon="money"
-                text="Total consolidado"
-                number="R$ 0"
-              />
-            </Col>
+            {infoBoxesToRender.map((v, i) => (
+              <Col {...v.row} key={i}>
+                <InfoBox {...v.info} />
+              </Col>
+            ))}
           </Row>
         </Content.Body>
       </Content>
     )
   }
 }
-export default Dashboard
+
+const mapStateToProps = state => ({ summary: state.dashboard.summary })
+const mapDispatchToProps = dispatch =>
+  bindActionCreators({ dataFetch }, dispatch)
+
+export { Dashboard }
+export default connect(mapStateToProps, mapDispatchToProps)(Dashboard)
--- my-money-app/frontend/src/index.js
+++ my-money-app/frontend/src/index.js
@@ -1,15 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import { Provider } from 'react-redux'
+import createSagaMiddleware from 'redux-saga'
 
 import reducers from './App/reducers'
+import rootSaga from './App/sagas'
 
 import './index.css'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 
-const store = createStore(reducers)
+const sagaMiddleware = createSagaMiddleware()
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const store = createStore(
+  reducers,
+  composeEnhancers(applyMiddleware(sagaMiddleware))
+)
+
+sagaMiddleware.run(rootSaga)
 
 ReactDOM.render(
   <Provider store={store}>
